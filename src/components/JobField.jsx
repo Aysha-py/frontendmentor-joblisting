1465e2e7ba4f7afc38fa58f3cebfc311244b70b5
Aysha Muhammed <m.aishat9890@gmail.com>
2022-06-14 09:04:08 -0700

    update

--- src/components/JobField.jsx
+++ src/components/JobField.jsx
@@ -15,38 +15,46 @@ const JobField = ({information,listingData}) => {
                         <div className='jobfield-content'>
                            <div className='jobfield-image'>
                                 <img src={value?.logo}    alt="logo"/>
-                            </div> 
-                            <div className='jobfield-details'>
-                                <div className='jobfield-details-title'>
-                                    <p>{value?.company}</p>
-                                </div>
-                                <div className='jobfield-details-title-subtitle'>
-                                { index === 0 || index === 1 || index === 2 ?
-                                     <p className='p1'>New !   </p> : null
-                                }
-                                </div>
-                                <div className='jobfield-details-title-subtitle'>
-                                { index === 0 || index === 1 ?
-                                    <p className='p2'>Featured</p> : null
-                                 
-                                 }
-                                </div>
                             </div>
-                        </div>   
+                            <div className="hhh">
+                                <div className='jobfield-details'>
+                                    <div className='jobfield-details-title'>
+                                        <p>{value?.company}</p>
+                                    </div>
+                                    <div className='jobfield-details-title-subtitle'>
+                                        { index === 0 || index === 1 || index === 2 ?
+                                            <p className='p1'>New !   </p> : null
+                                        }
+                                    </div>
+                                    <div className='jobfield-details-title-subtitle'>
+                                        { index === 0 || index === 1 ?
+                                            <p className='p2'>Featured</p> : null
+                                        }
+                                    </div>
 
-                        <div className='jobfield-details-position'>
-                            <p>{value?.position}</p>
-                        </div>
+                                </div>
+                                    
+                                <div className='jobfield-other-info'>
+                                    <div className='jobfield-details-position'>
+                                        <p>{value?.position}</p>
 
-                        <div className='jobfield-details-location'> 
-                            <p>{value?.postedAt}.</p>
-                            <div className='jobfield-details-location-subtitle'>
-                                <p>{value?.contract} .</p>
-                            </div>
-                            <div className='jobfield-details-location-subtitle'>
-                                <p>{value?.location}.</p>
-                            </div>
-                        </div>
+                                    </div>
+                                    <div className='jobfield-details-location'> 
+                                        <p>{value?.postedAt}.</p>
+                                        <div className='jobfield-details-location-subtitle'>
+                                            <p>{value?.contract} .</p>
+                                        </div>
+                                        <div className='jobfield-details-location-subtitle'>
+                                            <p>{value?.location}.</p>
+                                        </div>
+                                    </div>
+                                   
+                                </div>     
+                            </div>     
+                                
+                            
+                          
+                        </div> 
                     </div> 
 
               
